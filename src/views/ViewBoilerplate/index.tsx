018809e0cd0b107db9b5a121a56bd4e064d052fb
Vinícius Scala <viniciusscala@gmail.com>
2021-07-27 22:07:30 -0300

    (develop): make the base request

--- src/views/ViewBoilerplate/index.tsx
+++ src/views/ViewBoilerplate/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
-import { Upload, Button } from 'antd';
+// import { Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import axios from 'axios';
 import { Container, Card } from './styles';
 
 function ViewBoilerplate(): ReactElement {
@@ -10,9 +11,25 @@ function ViewBoilerplate(): ReactElement {
         <header>
           Upload do arquivo
         </header>
-        <Upload action="http://127.0.0.1:8000/files" directory>
+        {/* <Upload action="http://127.0.0.1:8000/files" directory>
           <Button icon={<UploadOutlined />}>Enviar diretório</Button>
-        </Upload>
+        </Upload> */}
+        <button
+          onClick={async () => {
+            try {
+              const response = await axios.post('http://127.0.0.1:8000/files', {
+                file_name: 'patch_3',
+                file_extension: 'raw',
+              });
+              console.log(response, 'resposta');
+            } catch (error) {
+              console.log(error);
+            }
+          }}
+          type="button"
+        >
+          teste
+        </button>
       </Card>
     </Container>
   );
